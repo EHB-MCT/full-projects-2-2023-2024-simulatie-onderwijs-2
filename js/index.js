a1bb8e6c870acd62f6b9925700c48e5e1113f288
Meo Klaklang <siriyakorn.klaklang@student.ehb.be>
2024-06-13 11:27:01 +0200

    pxl pagina

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
+console.log("hello world");
 
-console.log("hello world")
+document.addEventListener("DOMContentLoaded", function () {
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+				panel.style.display = "none";
+			} else {
+				panel.style.display = "block";
+			}
+		});
+	}
+});
