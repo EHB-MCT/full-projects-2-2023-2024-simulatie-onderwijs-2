8a52a60a02293c592ac97c10812211dbe936f28a
eren sahbaz <eren.sahbaz@student.ehb.be>
2024-06-18 15:10:20 +0200

    test

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+document
+	.getElementById("contactForm")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
+
+		const formData = {
+			firstName: document.getElementById("firstName").value,
+			lastName: document.getElementById("lastName").value,
+			email: document.getElementById("email").value,
+			phone: document.getElementById("phone").value,
+			message: document.getElementById("message").value,
+		};
+
+		console.log("Form data:", formData);
+
+		fetch(
+			"https://simulatie2.pockethost.io/api/collections/contact_us/records",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			}
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Response from server:", data);
+				alert("Message sent successfully!");
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+				alert("Error sending message!");
+			});
+	});
